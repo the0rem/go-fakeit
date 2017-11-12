bd56e0ee3461904b15f0e698855c8e3f68f26f08
Shaun Smekel <shaun.smekel@theorem.net.au>
2017-11-12 16:32:56 +1100

    Added docblocks to fakers
    Signed-off-by: Shaun Smekel <shaun.smekel@theorem.net.au>

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
 # go-fakeit
 Go Fakeit designed to work with go-openapi/strfmt and Struct field tag interpolation
 
+
+github.com/Pallinder/go-randomdata
+
 [![Go Report Card](https://goreportcard.com/badge/github.com/the0rem/go-fakeit)](https://goreportcard.com/report/github.com/the0rem/go-fakeit)
 [![Coverage Status](https://coveralls.io/repos/github/the0rem/go-fakeit/badge.svg?branch=master)](https://coveralls.io/github/the0rem/go-fakeit?branch=master)
 [![Godoc](https://godoc.org/github.com/the0rem/go-fakeit?status.svg)](https://godoc.org/github.com/the0rem/go-fakeit)
 [![Release](https://img.shields.io/github/release/the0rem/go-fakeit.svg)](https://github.com/the0rem/go-fakeit/releases/latest)
 
-##TODO
-[ ] Handle Interface & Map Struct Fields
-[ ] Complete test coverage for handlers and fakers
-[ ] Add godocs for public functions
-[X] Add handling for limiting recursion depth of circular dependencies
-[X] Add Struct Field Tag option to omit Field from being faked
+## TODO
+
+- [ ] Handle Interface & Map Struct Fields
+- [ ] Complete test coverage for handlers and fakers
+- [ ] Add godocs for public functions
+- [X] Add handling for limiting recursion depth of circular dependencies
+- [X] Add Struct Field Tag option to omit Field from being faked
+
+## Available Fakers
+Fakeit implements the available fakers in the https://github.com/icrowley/fake package. Available fakers can be found  on [Godoc](https://godoc.org/github.com/the0rem/go-fakeit/fakers/)
+
+## Using tags to specify faker rules
+The `fakeit` Tag can be used on Struct Fields to specify faker behaviour. The examples below describe how to utilise the functionality.
+
+### Example 1 - Overriding the default faker:
+
+```
+type AwesomeThing struct {
+  ID            strfmt.UUID
+  Name          string `fakeit:"password,min=25,max=30"`
+  Description   *string `fakeit:"paragraph"`
+}
+```
+
+### Example 2 - Omitting a Field form being faked:
+There are cases where you don't want to populate a certain field in a struct (such as when generating data for inserting into a database). In this case the field can be annotated with a hyphen (`-`). This follows the convention set the the JSON package in Go.
+```
+type AwesomeThing struct {
+  ID        strfmt.UUID
+  Name      string
+  DeletedAt *string `fakeit:"-"`
+}
+```
+The result will be:
+```
+thing := AwesomeThing{}
+fakeit.FakeIt(&thing)
+fmt.Printf("%+v\n", thing)
+
+// Prints output:
+// &{ID:34b7a0d3-c753-11e7-9dd1-dca90491e849 Name:Stagorchid DeletedAt:<nil>}
+```
--- fakers/fakers.go
+++ fakers/fakers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/icrowley/fake"
 )
 
+// NewFakers returns a map of the available fakers.
 func NewFakers() map[string]func(args ...interface{}) interface{} {
 	return map[string]func(args ...interface{}) interface{}{
 		"brand":                    Brand,
@@ -105,18 +106,22 @@ func NewFakers() map[string]func(args ...interface{}) interface{} {
 	}
 }
 
+// Brand generates a brand name
 func Brand(args ...interface{}) interface{} {
 	return fake.Brand()
 }
 
+// Character generates a random character
 func Character(args ...interface{}) interface{} {
 	return fake.Character()
 }
 
+// Characters generates from 1 to 5 characters in the given language
 func Characters(args ...interface{}) interface{} {
 	return fake.Characters()
 }
 
+// CharactersN generates n random characters in the given language
 func CharactersN(args ...interface{}) interface{} {
 	number := 1
 	if len(args) > 0 {
@@ -125,30 +130,38 @@ func CharactersN(args ...interface{}) interface{} {
 	return fake.CharactersN(number)
 }
 
+// City generates random city
 func City(args ...interface{}) interface{} {
 	return fake.City()
 }
 
+// Color generates color name
 func Color(args ...interface{}) interface{} {
 	return fake.Color()
 }
 
+// Company generates company name
 func Company(args ...interface{}) interface{} {
 	return fake.Company()
 }
 
+// Continent generates random continent
 func Continent(args ...interface{}) interface{} {
 	return fake.Continent()
 }
 
+// Country generates random country
 func Country(args ...interface{}) interface{} {
 	return fake.Country()
 }
 
+// CreditCardType returns one of the following credit values:
+// VISA, MasterCard, American Express and Discover
 func CreditCardType(args ...interface{}) interface{} {
 	return fake.CreditCardType()
 }
 
+// CreditCardNum generated credit card number according to the card number rules
 func CreditCardNum(args ...interface{}) interface{} {
 	provider := ""
 	if len(args) > 0 {
@@ -157,6 +170,7 @@ func CreditCardNum(args ...interface{}) interface{} {
 	return fake.CreditCardNum(provider)
 }
 
+// Cron generates random CRON value
 func Cron(args ...interface{}) interface{} {
 	minute := rand.Intn(59-0) + 0
 	hour := rand.Intn(23-0) + 0
@@ -166,22 +180,27 @@ func Cron(args ...interface{}) interface{} {
 	return fmt.Sprintf("%d %d %d %d %d", minute, hour, monthDay, month, weekDay)
 }
 
+// Currency generates currency name
 func Currency(args ...interface{}) interface{} {
 	return fake.Currency()
 }
 
+// CurrencyCode generates currency code
 func CurrencyCode(args ...interface{}) interface{} {
 	return fake.CurrencyCode()
 }
 
+// Day generates day of the month
 func Day(args ...interface{}) interface{} {
 	return fake.Day()
 }
 
+// Digits returns from 1 to 5 digits as a string
 func Digits(args ...interface{}) interface{} {
 	return fake.Digits()
 }
 
+// DigitsN returns n digits as a string
 func DigitsN(args ...interface{}) interface{} {
 	number := 1
 	if len(args) > 0 {
@@ -190,14 +209,17 @@ func DigitsN(args ...interface{}) interface{} {
 	return fake.DigitsN(number)
 }
 
+// DomainName generates random domain name
 func DomainName(args ...interface{}) interface{} {
 	return fake.DomainName()
 }
 
+// DomainZone generates random domain zone
 func DomainZone(args ...interface{}) interface{} {
 	return fake.DomainZone()
 }
 
+// Enum generates random string from enum values
 func Enum(args ...interface{}) interface{} {
 	var enum []string
 	for _, arg := range args {
@@ -208,186 +230,241 @@ func Enum(args ...interface{}) interface{} {
 	return enum[n]
 }
 
+// EmailAddress generates email address
 func EmailAddress(args ...interface{}) interface{} {
 	return fake.EmailAddress()
 }
 
+// EmailBody generates random email body
 func EmailBody(args ...interface{}) interface{} {
 	return fake.EmailBody()
 }
 
+// EmailSubject generates random email subject
 func EmailSubject(args ...interface{}) interface{} {
 	return fake.EmailSubject()
 }
 
+// FemaleFirstName generates female first name
 func FemaleFirstName(args ...interface{}) interface{} {
 	return fake.FemaleFirstName()
 }
 
+// FemaleFullName generates female full name
+// it can occasionally include prefix or suffix
 func FemaleFullName(args ...interface{}) interface{} {
 	return fake.FemaleFullName()
 }
 
+// FemaleFullNameWithPrefix generates prefixed female full name
+// if prefixes for the given language are available
 func FemaleFullNameWithPrefix(args ...interface{}) interface{} {
 	return fake.FemaleFullNameWithPrefix()
 }
 
+// FemaleFullNameWithSuffix generates suffixed female full name
+// if suffixes for the given language are available
 func FemaleFullNameWithSuffix(args ...interface{}) interface{} {
 	return fake.FemaleFullNameWithSuffix()
 }
 
+// FemaleLastName generates female last name
 func FemaleLastName(args ...interface{}) interface{} {
 	return fake.FemaleLastName()
 }
 
+// FemalePatronymic generates female patronymic
 func FemalePatronymic(args ...interface{}) interface{} {
 	return fake.FemalePatronymic()
 }
 
+// FirstName generates first name
 func FirstName(args ...interface{}) interface{} {
 	return fake.FirstName()
 }
 
+// FullName generates full name
+// it can occasionally include prefix or suffix
 func FullName(args ...interface{}) interface{} {
 	return fake.FullName()
 }
 
+// FullNameWithPrefix generates prefixed full name
+// if prefixes for the given language are available
 func FullNameWithPrefix(args ...interface{}) interface{} {
 	return fake.FullNameWithPrefix()
 }
 
+// FullNameWithSuffix generates suffixed full name
+// if suffixes for the given language are available
 func FullNameWithSuffix(args ...interface{}) interface{} {
 	return fake.FullNameWithSuffix()
 }
 
+// Gender generates random gender
 func Gender(args ...interface{}) interface{} {
 	return fake.Gender()
 }
 
+// GenderAbbrev returns first downcased letter of the random gender
 func GenderAbbrev(args ...interface{}) interface{} {
 	return fake.GenderAbbrev()
 }
 
+// HexColor generates hex color name
 func HexColor(args ...interface{}) interface{} {
 	return fake.HexColor()
 }
 
+// HexColorShort generates short hex color name
 func HexColorShort(args ...interface{}) interface{} {
 	return fake.HexColorShort()
 }
 
+// IPv4 generates IPv4 address
 func IPv4(args ...interface{}) interface{} {
 	return fake.IPv4()
 }
 
+// IPv6 generates IPv6 address
 func IPv6(args ...interface{}) interface{} {
 	return fake.IPv6()
 }
 
+// Industry generates industry name
 func Industry(args ...interface{}) interface{} {
 	return fake.Industry()
 }
 
+// JobTitle generates job title
 func JobTitle(args ...interface{}) interface{} {
 	return fake.JobTitle()
 }
 
+// Language generates random human language
 func Language(args ...interface{}) interface{} {
 	return fake.Language()
 }
 
+// LastName generates last name
 func LastName(args ...interface{}) interface{} {
 	return fake.LastName()
 }
 
+// Latitude generates latitude (from -90.0 to 90.0)
 func Latitude(args ...interface{}) interface{} {
 	return fake.Latitude()
 }
 
+// LatitudeDegrees generates latitude degrees (from -90 to 90)
 func LatitudeDegrees(args ...interface{}) interface{} {
 	return fake.LatitudeDegrees()
 }
 
+// LatitudeDirection generates latitude direction (N(orth) o S(outh))
 func LatitudeDirection(args ...interface{}) interface{} {
 	return fake.LatitudeDirection()
 }
 
+// LatitudeMinutes generates latitude minutes (from 0 to 60)
 func LatitudeMinutes(args ...interface{}) interface{} {
 	return fake.LatitudeMinutes()
 }
 
+// LatitudeSeconds generates latitude seconds (from 0 to 60)
 func LatitudeSeconds(args ...interface{}) interface{} {
 	return fake.LatitudeSeconds()
 }
 
+// Longitude generates longitude (from -180 to 180)
 func Longitude(args ...interface{}) interface{} {
 	return fake.Longitude()
 }
 
+// LongitudeDegrees generates longitude degrees (from -180 to 180)
 func LongitudeDegrees(args ...interface{}) interface{} {
 	return fake.LongitudeDegrees()
 }
 
+// LongitudeDirection generates (W(est) or E(ast))
 func LongitudeDirection(args ...interface{}) interface{} {
 	return fake.LongitudeDirection()
 }
 
+// LongitudeMinutes generates (from 0 to 60)
 func LongitudeMinutes(args ...interface{}) interface{} {
 	return fake.LongitudeMinutes()
 }
 
+// LongitudeSeconds generates (from 0 to 60)
 func LongitudeSeconds(args ...interface{}) interface{} {
 	return fake.LongitudeSeconds()
 }
 
+// MaleFirstName generates male first name
 func MaleFirstName(args ...interface{}) interface{} {
 	return fake.MaleFirstName()
 }
 
+// MaleFullName generates male full name
+// it can occasionally include prefix or suffix
 func MaleFullName(args ...interface{}) interface{} {
 	return fake.MaleFullName()
 }
 
+// MaleFullNameWithPrefix generates prefixed male full name
+// if prefixes for the given language are available
 func MaleFullNameWithPrefix(args ...interface{}) interface{} {
 	return fake.MaleFullNameWithPrefix()
 }
 
+// MaleFullNameWithSuffix generates suffixed male full name
+// if suffixes for the given language are available
 func MaleFullNameWithSuffix(args ...interface{}) interface{} {
 	return fake.MaleFullNameWithSuffix()
 }
 
+// MaleLastName generates male last name
 func MaleLastName(args ...interface{}) interface{} {
 	return fake.MaleLastName()
 }
 
+// MalePatronymic generates male patronymic
 func MalePatronymic(args ...interface{}) interface{} {
 	return fake.MalePatronymic()
 }
 
+// Model generates model name that consists of letters and digits, optionally with a hyphen between them
 func Model(args ...interface{}) interface{} {
 	return fake.Model()
 }
 
+// Month generates month name
 func Month(args ...interface{}) interface{} {
 	return fake.Month()
 }
 
+// MonthNum generates month number (from 1 to 12)
 func MonthNum(args ...interface{}) interface{} {
 	return fake.MonthNum()
 }
 
+// MonthShort generates abbreviated month name
 func MonthShort(args ...interface{}) interface{} {
 	return fake.MonthShort()
 }
 
+// Paragraph generates paragraph
 func Paragraph(args ...interface{}) interface{} {
 	return fake.Paragraph()
 }
 
+// Paragraphs generates from 1 to 5 paragraphs
 func Paragraphs(args ...interface{}) interface{} {
 	return fake.Paragraphs()
 }
 
+// ParagraphsN generates n paragraphs
 func ParagraphsN(args ...interface{}) interface{} {
 	number := 1
 	if len(args) > 0 {
@@ -396,6 +473,8 @@ func ParagraphsN(args ...interface{}) interface{} {
 	return fake.ParagraphsN(number)
 }
 
+// Password generates password with the length from atLeast to atMost charachers,
+// allow* parameters specify whether corresponding symbols can be used
 func Password(args ...interface{}) interface{} {
 	var from int
 	from = 6
@@ -427,30 +506,37 @@ func Password(args ...interface{}) interface{} {
 	return fake.Password(from, to, allowUpper, allowNumeric, allowSpecial)
 }
 
+// Patronymic generates patronymic
 func Patronymic(args ...interface{}) interface{} {
 	return fake.Patronymic()
 }
 
+// Phone generates random phone number using one of the formats format specified in phone_format file
 func Phone(args ...interface{}) interface{} {
 	return fake.Phone()
 }
 
+// Product generates product title as brand + product name
 func Product(args ...interface{}) interface{} {
 	return fake.Product()
 }
 
+// ProductName generates product name
 func ProductName(args ...interface{}) interface{} {
 	return fake.ProductName()
 }
 
+// Sentence generates random sentence
 func Sentence(args ...interface{}) interface{} {
 	return fake.Sentence()
 }
 
+// Sentences generates from 1 to 5 random sentences
 func Sentences(args ...interface{}) interface{} {
 	return fake.Sentences()
 }
 
+// SentencesN generates n random sentences
 func SentencesN(args ...interface{}) interface{} {
 	number := 1
 	if len(args) > 0 {
@@ -459,62 +545,79 @@ func SentencesN(args ...interface{}) interface{} {
 	return fake.SentencesN(number)
 }
 
+// SimplePassword is a wrapper around Password,
+// it generates password with the length from 6 to 12 symbols, with upper characters and numeric symbols allowed
 func SimplePassword(args ...interface{}) interface{} {
 	return fake.SimplePassword()
 }
 
+// State generates random state
 func State(args ...interface{}) interface{} {
 	return fake.State()
 }
 
+// StateAbbrev generates random state abbreviation
 func StateAbbrev(args ...interface{}) interface{} {
 	return fake.StateAbbrev()
 }
 
+// Street generates random street name
 func Street(args ...interface{}) interface{} {
 	return fake.Street()
 }
 
+// StreetAddress generates random street name along with building number
 func StreetAddress(args ...interface{}) interface{} {
 	return fake.StreetAddress()
 }
 
+// Title generates from 2 to 5 titleized words
 func Title(args ...interface{}) interface{} {
 	return fake.Title()
 }
 
+// TopLevelDomain generates random top level domain
 func TopLevelDomain(args ...interface{}) interface{} {
 	return fake.TopLevelDomain()
 }
 
+// UserAgent generates a random user agent
 func UserAgent(args ...interface{}) interface{} {
 	return fake.UserAgent()
 }
 
+// UserName generates user name in one of the following forms
+// first name + last name, letter + last names or concatenation of from 1 to 3 lowercased words
 func UserName(args ...interface{}) interface{} {
 	return fake.UserName()
 }
 
+// WeekDay generates name ot the week day
 func WeekDay(args ...interface{}) interface{} {
 	return fake.WeekDay()
 }
 
+// WeekDayNum generates number of the day of the week
 func WeekDayNum(args ...interface{}) interface{} {
 	return fake.WeekdayNum()
 }
 
+// WeekDayShort generates abbreviated name of the week day
 func WeekDayShort(args ...interface{}) interface{} {
 	return fake.WeekDayShort()
 }
 
+// Word generates random word
 func Word(args ...interface{}) interface{} {
 	return fake.Word()
 }
 
+// Words generates from 1 to 5 random words
 func Words(args ...interface{}) interface{} {
 	return fake.Words()
 }
 
+// WordsN generates n random words
 func WordsN(args ...interface{}) interface{} {
 	number := 1
 	if len(args) > 0 {
@@ -523,6 +626,7 @@ func WordsN(args ...interface{}) interface{} {
 	return fake.WordsN(number)
 }
 
+// Year generates year using the given boundaries
 func Year(args ...interface{}) interface{} {
 	from := time.Now().Year() - 10
 	if len(args) > 0 {
@@ -537,6 +641,7 @@ func Year(args ...interface{}) interface{} {
 	return fake.Year(from, to)
 }
 
+// Zip generates random zip code using one of the formats specifies in zip_format file
 func Zip(args ...interface{}) interface{} {
 	return fake.Zip()
 }
